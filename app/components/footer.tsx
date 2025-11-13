6d9468bedc60d317120411932621cf516a0ab5da
ConstintCreations <ethanglenn1208@gmail.com>
2025-11-12 20:13:29 -0600

    Title Screen with Bounce Animation, Footer, and Favicon

---
+++ app/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <div className="flex justify-center items-center h-8 w-screen bg-gray-800">
+            <p>All trademarks, logos, and images are the property of their respective owners.</p>
+        </div>
+    )
+}
